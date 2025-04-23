afdc59b36c994958f5d4c4366f0bb37c44744c8e
ADITYA CHAUHAN <137919490+Aditya-chauhann@users.noreply.github.com>
2025-04-23 14:50:29 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#element', {
+  strings: ['Web Developer', 'Python Developer', 'Cloud Computing Enthusiast'],
+  typeSpeed: 50,
+  backSpeed: 25, 
+  loop: true 
+});
+
+  
+  
